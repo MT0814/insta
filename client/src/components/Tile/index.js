6b83287a994ee9a30b13529d1ee9971b40ac966d
MT0814 <millie_tsai@me.com>
2022-01-23 11:17:57 -0800

    added commnet (done) and need to able to connect to comment list and comment page

--- client/src/components/Tile/index.js
+++ client/src/components/Tile/index.js
@@ -1,31 +1,65 @@
 import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import millie from '../../images/millie.JPG'
 import im2 from '../../images/im1.png'
 import { Card, Icon, Image } from 'semantic-ui-react'
 
 function Tile({ thought, showUsername }) {
     return (
-        <Card>
+        <Card key={thought._id}>
             <Card.Content>
-                <Image className="ui avatar image" src={millie} wrapped ui={false} />Millie
+                <Image className="ui avatar image" src={millie} wrapped ui={false} />User Name
                 {/* <Card.Header>Millie</Card.Header> */}
             </Card.Content>
             <Image src={im2} wrapped ui={false} />
-            <Card.Content>
-                {/* <Card.Meta>
+            {/* <Card.Content> */}
+
+            {/* <Card.Meta>
                     <span className='date'>Joined in 2015</span>
                 </Card.Meta> */}
 
-                <Card.Description>
-                    Matthew is a musician living in Nashville.
-                </Card.Description>
-            </Card.Content>
+            {/* <Card.Description>
+                    {showUsername ? (
+                        <Link
+                            className="text-light"
+                            to={`/profiles/${thought.thoughtAuthor}`}
+                        >
+                            {thought.thoughtAuthor} <br />
+                            <span style={{ fontSize: '1rem' }}>
+                                had this thought on {thought.createdAt}
+                            </span>
+                        </Link>
+                    ) : (
+                        <>
+                            <span style={{ fontSize: '1rem' }}>
+                                You had this thought on {thought.createdAt}
+                            </span>
+                        </>
+                    )}
+                </Card.Description> */}
+            {/* </Card.Content> */}
+
             <Card.Content extra>
+                <span className="right floated">
+                    <a>
+                        <Icon name='like' />4 Likes
+                    </a>
+                </span>
                 <a>
-                    <Icon name='like' />4 Likes
+                    <Link className="submit button" to="/commentform">
+                        <Icon name='comment' />4 Comments
+                    </Link>
+
                 </a>
             </Card.Content>
+            {/* 
+            <Card.Content extra>
+
+                <Input placeholder="Add Comment..." />
+
+            </Card.Content> */}
+
+
         </Card >
 
 
